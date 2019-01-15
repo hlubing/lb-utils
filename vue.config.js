0d0492c295eeb95823b207ece5c5ed99b5037c44
lvbing <lvbing@zhongan.com>
2019-01-15 15:41:39 +0800

    修改

--- vue.config.js
+++ vue.config.js
@@ -1,27 +1,4 @@
 module.exports = {
-  // devServer: {
-  //   host: 'localhost',
-  //   hot: true,
-  //   proxy: {
-  //     '/api': {
-  //       target: 'https://diandian-test2.zhongan.com/apiAction.do',
-  //       changeOrigin: true,
-  //       pathRewrite: {
-  //         '^/api': ''
-  //       }
-  //     },
-  //     '/mallapi': {
-  //       target: 'https://ddm.zhongan.com',
-  //       changeOrigin: true,
-  //       pathRewrite: {
-  //         '^/mallapi': ''
-  //       }
-  //     }
-  //   },
-  //   before: app => {
-  //     console.log(app)
-  //   }
-  // },
   baseUrl: '/lb-utils/',
   configureWebpack: () => {
     return {
